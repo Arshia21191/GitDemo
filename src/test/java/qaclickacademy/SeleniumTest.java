78fafa7abe408955a02db705fecd40bd9911eba0
arshia <arshia.catalystone@gmail.com>
2019-07-25 09:45:22 +0530

    first commit

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,23 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+import org.testng.collections.*;
+
+public class SeleniumTest {
+	//TEST XML file from Maven
+	//how to execute all test cases from test folder -mvn test
+	
+@Test
+
+public void BrowserAutomation()
+{
+	System.out.println("BrowserAutomation");
+}
+
+@Test
+
+public void elementsUI()
+{
+	System.out.println("elementsUI");
+}
+}
